a3d7b66a0143a9ceb9e0f8e1ae7d175d2cef167e
prassu56 <prasuprasanna236@gmail.com>
2024-12-07 14:10:14 +0530

    Add files via upload

---
+++ ArrayMultiplication.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class ArrayMultiplication{
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        int s=1;
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+            s*=arr[i];
+        }
+            System.out.println(s);
+        }
+        
+    }
